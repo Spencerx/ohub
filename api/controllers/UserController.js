ef8c37a4b30552a602a4dce2ac2d631267923554
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2017-02-27 17:20:33 +0100

    Find user by username

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,4 +7,35 @@
 
 module.exports = {
 
+  find: function(req, res) {
+
+    if (req.query.username) {
+      User.findOne({ username: req.query.username })
+        .populate("projects")
+        .exec(function(err, user) {
+          if (err) {
+            sails.log.error(err);
+            return res.serverError({ error: 'Internal server error'});
+          }
+
+          if (!user) {
+            return res.send(404);
+          }
+
+          res.ok(user);
+        });
+    }
+    else {
+      User.find()
+        .populate("projects")
+        .exec(function(err, users) {
+          if (err) {
+            sails.log.error(err);
+            return res.serverError({ error: 'Internal server error'});
+          }
+          res.ok(users);
+        });
+    }
+  },
+
 };
